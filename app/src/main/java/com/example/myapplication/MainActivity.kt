e3775b40ef927125aa4e1f48469776ec9ee5c59a
student <a0025942@gmail.com>
2023-09-15 09:22:51 +0200

    ajjaajajjajjaj

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.Mojprzycisk).setOnClickListener {
-            findViewById<Button>(R.id.Mojprzycisk).text = "Działa!!!!!!"
+            findViewById<Button>(R.id.Mojprzycisk).text = "chyba dziala"
+            findViewById<TextInputEditText>(R.id.MojTekstEdit).text
         }
     }
 }
